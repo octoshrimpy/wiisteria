7c5cb4ad08ab9308b175d9bece27e9cec373cc8a
octoshrimpy <octavo.acapella@gmail.com>
2019-02-14 01:12:46 -0700

    electron window, and livereload

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
 const {app, BrowserWindow} = require('electron')
+require('electron-reload')(__dirname)
+
+function createWindow() {
+  let window = new BrowserWindow({ width: 800, height: 600 })
+
+  window.loadFile('index.html')
+
+  window.webContents.openDevTools()
+
+  window.on('closed', () => {
+    window = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('activate', () => {
+
+  if(window === null) {
+    createWindow()
+  }
+})
+
+
+
+
+
+
+
+
+
+//
